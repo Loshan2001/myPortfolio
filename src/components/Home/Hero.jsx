1ba4adcd2999e2a1b6ffc55821e12e21cbb2abe2
loshan <it21803970@my.sliit.lk>
2024-08-01 18:42:00 +0530

    fixed error

---
+++ src/components/Home/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Hero.css'
+function Hero() {
+  return (
+   <div className='max-w-full max-h-screen grid grid-cols-3'>
+        <div>Details</div>
+        <div>Hero</div>
+        <div>Sidebar</div>
+        <div>Project Header</div>
+        <div>Project</div>
+   </div>
+  )
+}
+
+export default Hero
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
- /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary : '#FFB400',  //dark yellow
+        yellow : 'FFB80D',    //light yellow
+        light : '#F1F0F6',
+        white : '#FFFFFF',
+        gray : '767676',
+        dark:'#2B2B2B',
+        black : '18191A'
+      }
+    },
+    gridTemplateAreas : {
+      'custom-grid' : ' "details hero sidebar" "details p-header sidebar" "details project sidebar"'
+    }
   },
-  plugins: [],
-}
+  plugins: [
+    function({ addUtilities, e, theme }) {
+      const areas = theme('gridTemplateAreas', {});
+      const utilities = Object.keys(areas).map(key => {
+        return {
+          [`.${e(`grid-template-areas-${key}`)}`]: {
+            'grid-template-areas': areas[key],
+          }
+        };
+      });
+      addUtilities(utilities, ['responsive', 'hover']);
+    }
+  ],
+};
